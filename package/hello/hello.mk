cfbca6d3403111c1b25ba4afe928222edad6ca2a
old zhu <wp_xdzhu@hotmail.com>
2020-08-26 11:17:48 +0800

    add hello packagewq

---
+++ package/hello/hello.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+## hello
+#
+#################################################################################
+
+HELLO_VERSION = 1.0
+HELLO_SITE = ~/4dotnet/package/hello/src
+HELLO_SITE_METHOD = local
+
+define HELLO_BUILD_CMDS
+    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
+endef
+
+define HELLO_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/hello $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
