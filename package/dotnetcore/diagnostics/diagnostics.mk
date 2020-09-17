cf736ad0cf7a8180603fd1a7a7979c80239868e5
oldzhu <wp_xdzhu@hotmail.com>
2020-09-17 19:50:27 +0800

    modify diagnostics.mk to disable sourelink and export TOOLCHAIN

--- package/dotnetcore/diagnostics/diagnostics.mk
+++ package/dotnetcore/diagnostics/diagnostics.mk
@@ -12,7 +12,7 @@ DIAGNOSTICS_SITE = git://github.com/dotnet/diagnostics.git
 DIAGNOSTICS_INSTALL_TARGET = YES
 
 define DIAGNOSTICS_BUILD_CMDS
-export PATH=$(BUILD_DIR)/host-lldb-origin_master/llvm/buildroot-build/bin:$$PATH:$(HOST_DIR)/bin;$(@D)/build.sh --architecture $(BR2_PACKAGE_DIAGNOSTICS_TARGET_ARCH) --rootfs  $(STAGING_DIR) 
+export PATH=$(BUILD_DIR)/host-lldb-origin_master/llvm/buildroot-build/bin:$$PATH:$(HOST_DIR)/bin;export TOOLCHAIN=arm-buildroot-linux-gnueabihf;$(@D)/build.sh --architecture $(BR2_PACKAGE_DIAGNOSTICS_TARGET_ARCH) --rootfs  $(STAGING_DIR) /p:EnableSourceLink=false
 endef
 
 define DIAGNOSTICS_INSTALL_TARGET_CMDS
