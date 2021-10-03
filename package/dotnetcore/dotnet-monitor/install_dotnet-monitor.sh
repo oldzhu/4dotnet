3fe0fcb6fe5503326bf216a50f8c15ebbcd024b3
oldzhu <wp_xdzhu@hotmail.com>
2021-10-03 17:12:51 +0800

    adding dotnet-monitor package

---
+++ package/dotnetcore/dotnet-monitor/install_dotnet-monitor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# p1 BUILD_DIR
+# p2 HOST_DIR
+# p3 BR2_PACKAGE_{PKGNAME}_TARGET_ARCH
+# p4 @D
+# p5 STAGING_DIR
+# p6 [PKGNAME}_PKGDIR
+# p7 TARGET_DIR
+
+echo "install dotnet-monitor"
+
+#if [ $3 == "ARM64" ]; then
+#	mkdir -p $7/root/.dotnet/sos
+#	cp -v $4/artifacts/bin/Linux.aarch64.Debug/{libsos.so,libsosplugin.so,sosdocsunix.txt} \
+#	$7/root/.dotnet/sos
+#elif [ $3 == "ARM" ]; then
+#	mkdir -p $7/root/.dotnet/sos
+#	cp -v $4/artifacts/bin/Linux.arm.Debug/{libsos.so,libsosplugin.so,sosdocsunix.txt} \
+#	$7/root/.dotnet/sos
+#else
+#	mkdir -p $7/root/.dotnet/sos
+#        cp -v $4/artifacts/bin/Linux.x86_64.Debug/{libsos.so,libsosplugin.so,sosdocsunix.txt} \
+#        $7/root/.dotnet/sos
+#fi
