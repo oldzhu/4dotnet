1dd14b7402e63afa1b71f44d059e5ccc4438e95c
oldzhu <wp_xdzhu@hotmail.com>
2024-01-22 08:25:00 +0800

    1. modfied he br2 defconfig based on the new qemu_riscv32_virt_defconfig and qemu_riscv64_virt_defconfig 2. added the qemu starting script for riscv64

---
+++ scripts/riscv64/start-qemu.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+#echo $SCRIPTPATH
+BUILDROOTPATH=$SCRIPTPATH/../../../buildroot
+$BUILDROOTPATH/output/host/bin/qemu-system-riscv64 -M virt -smp 2 -m 2048 -nographic -bios $BUILDROOTPATH/output/images/fw_jump.elf -kernel $BUILDROOTPATH/output/images/Image -drive file=$BUILDROOTPATH/output/images/rootfs.ext2,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -append "rootwait root=/dev/vda" -device virtio-net-device,netdev=eth0 -netdev user,id=eth0,hostfwd=tcp::2222-:22 -fsdev local,id=v_9p_dev,path=$BUILDROOTPATH,security_model=none -device virtio-9p-device,fsdev=v_9p_dev,mount_tag=hostshare "$@"
