fe120efb8375d1545d3ad8badd51e1acfae8a2b6
oldzhu <wp_xdzhu@hotmail.com>
2025-08-14 17:57:27 +0800

    1. backup config and build scripts for dotnetruntime and diagnostics package to *.001 2. change the previous way to build dotnetruntime and diagnostics with buildroot which is     copy c++ headers into myinclude folder and modify some projects CMakeList.txt to add myinclude     copy some c++ headers into rootfs/usr/include     now using the below environments to set the paths     CPLUS_INCLUDE_PATH     C_INCLUDE_PATH     LIBRARY_PATH     LDFLAGS

--- package/dotnetcore/diagnostics/config_diag.sh
+++ package/dotnetcore/diagnostics/config_diag.sh
@@ -16,45 +16,11 @@ else
 fi
 cplusplusver=$(cd $2/$toolchain/include/c++;echo *)
 
-function add_includes {
-	echo >> $1/$2
-	echo if\(\$ENV{CROSSCOMPILE} EQUAL 1\) >> $1/$2
-	echo include_directories\(\$ENV{HOST_DIR}/\$ENV{TOOLCHAIN}/include/c++/$cplusplusver\) >> $1/$2
-	echo include_directories\(\$ENV{HOST_DIR}/\$ENV{TOOLCHAIN}/include/c++/$cplusplusver/\$ENV{TOOLCHAIN}\) >> $1/$2
-	echo include_directories\(\$ENV{HOST_DIR}/\$ENV{TOOLCHAIN}/myinclude\) >> $1/$2
-	echo endif\(\) >> $1/$2
-}
-function apply_mypatches {
-        shopt -s globstar
-        for mypatchfile in $2/mypatches/*.mypatch
-	do
-	    patch -N -d $1 -p7 -u -b -i $mypatchfile
-	done
-}
 function copy_headslibs {
-        mkdir -p -v $1/$3/myinclude
-        cp -r -v $2/usr/include/{features.h,stdc-predef.h,sys,bits,gnu} $1/$3/myinclude
-        cp -r -v $1/$3/include/c++/$cplusplusver/{\
-type_traits,cstdlib,new,exception,bits,cstring,string,typeinfo,ext,set,debug,cwchar,\
-backward,cstdint,initializer_list,clocale,concepts,iosfwd,cctype,cstdio,cerrno,vector,\
-algorithm,utility,cstddef,cassert,limits,cinttypes,memory,tuple,array,mutex,chrono,\
-ratio,ctime,system_error,stdexcept,map,iostream,fstream,istream,ostream,cwctype,sstream,cstdarg,unordered_map,unordered_set,\
-climits,functional,locale,codecvt,iterator,list,atomic,condition_variable,thread,future,ios,streambuf,bit,cxxabi.h} \
-$1/$3/myinclude
         cp -u -v $1/lib/gcc/$3/$cplusplusver/{crtbegin.o,crtend.o,crtbeginS.o,crtendS.o,libgcc.a} $2/usr/lib
 }
 
-apply_mypatches $4 $6
-#add_includes $4 eng/cross/toolchain.cmake
 copy_headslibs $2 $5 $toolchain
-
-add_includes $4 src/SOS/extensions/CMakeLists.txt
-add_includes $4 src/shared/pal/src/CMakeLists.txt
-add_includes $4 src/shared/dbgutil/CMakeLists.txt
-add_includes $4 src/SOS/lldbplugin/CMakeLists.txt
-add_includes $4 src/SOS/Strike/CMakeLists.txt
-
-#cp -u -v $2/lib/gcc/$toolchain/$cplusplusver/{crtbegin.o,crtend.o,crtbeginS.o,crtendS.o,libgcc.a} $5/usr/lib
 
 mkdir -p -v $5/usr/include/lldb/API
 cp -u -v $2/include/lldb/API/* $5/usr/include/lldb/API
--- package/dotnetcore/dotnetruntime/build_dotnetruntime.sh
+++ package/dotnetcore/dotnetruntime/build_dotnetruntime.sh
@@ -12,17 +12,29 @@ export PATH=$hostlldbpath/llvm/buildroot-build/bin:$PATH:$2/bin;
 export ROOTFS_DIR=$5
 
 if [ $3 == "ARM" ]; then
-	export TOOLCHAIN=arm-buildroot-linux-gnueabihf;
+        export TOOLCHAIN=arm-buildroot-linux-gnueabihf;
 elif [ $3 == "ARM64" ]; then
-	export TOOLCHAIN=aarch64-buildroot-linux-gnu;
+        export TOOLCHAIN=aarch64-buildroot-linux-gnu;
 else
-	export TOOLCHAIN=x86_64-buildroot-linux-gnu;
+        export TOOLCHAIN=x86_64-buildroot-linux-gnu;
 fi
+
+cplusplusver=$(cd $2/$TOOLCHAIN/include/c++;echo *)
+
+export CPLUS_INCLUDE_PATH="$2/$TOOLCHAIN/include/c++/$cplusplusver:$2/$TOOLCHAIN/include/c++/$cplusplusver/$TOOLCHAIN:$2/$TOOLCHAIN/sysroot/usr/include"
+export C_INCLUDE_PATH="$2/$TOOLCHAIN/sysroot/usr/include"
+export LIBRARY_PATH="$2/lib/gcc/$TOOLCHAIN/$cplusplusver"
+export LDFLAGS="-L$2/lib/gcc/$TOOLCHAIN/$cplusplusver -Wl,-rpath=$2/lib/gcc/$TOOLCHAIN/$cplusplusver"
+
+# Preserve existing CMAKE_LIBRARY_PATH and APPEND new path LAST
+export CMAKE_LIBRARY_PATH="${CMAKE_LIBRARY_PATH}:/usr/lib/x86_64-linux-gnu"
 
 $4/build.sh \
 -subset clr+libs+host+packs \
 -arch $3 \
 -cross \
--c release
-#-v d 
+-c release \
+-cmakeargs "--debug-trycompile" \
+-cmakeargs "-DCMAKE_VERBOSE_MAKEFILE=ON"
+#-v d
 #/p:EnableSourceLink=false
